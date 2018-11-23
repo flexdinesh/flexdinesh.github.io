174b62b20cb91bbdd9a31a1fa9511e1b0908d6bd
AJ4Data <33787323+AJaafer@users.noreply.github.com>
2018-11-23 20:45:44 +0100

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -16,9 +16,9 @@ const LandingPage = (props, context) => {
       <main style={{ color: colorPrimary }}>
         <BrowserNotes />
         <div className="intro-wrapper">
-          <div className="intro-name">Hello, I'm Dinesh!</div>
+          <div className="intro-name">Hello, I'm Amani!</div>
           <div className="tagline">
-            Full Stack Dev | Open-Source Enthusiast | Blogger | Ninja
+            Data Scientist | Data Artist | Aîkidoka
           </div>
           <SocialIcons />
         </div>
